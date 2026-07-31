8c64bb3660fd5689d5994f33f0d01ff5fdb959b9
Anushka <anushkasaundane5@gmail.com>
2026-07-31 22:27:15 +0530

    changes made

--- pipes/views.py
+++ pipes/views.py
@@ -559,10 +559,11 @@ from django.contrib import messages
 from django.core.mail import EmailMessage
 from .models import ProductRequest 
 def request_product(request):
-    print("Request Method:", request.method)
+    
+    print("METHOD =", request.method)
 
     if request.method == "POST":
-        print("POST request received")
+        print("POST RECEIVED")
         
     if request.method == "POST":
 
